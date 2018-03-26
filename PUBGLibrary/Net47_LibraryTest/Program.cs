e45e04749e9c1dae6248dc19408d6fd3a0c88f3e
EpicKitten <applekitten@outlook.com>
2018-03-26 12:13:38 -0700

    Removed unused usings

--- PUBGLibrary/Net47_LibraryTest/Program.cs
+++ PUBGLibrary/Net47_LibraryTest/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using PUBGLibrary.API;
 
 namespace Net47_LibraryTest
